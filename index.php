6a9a586890debbdfcd2a5a820168d2706382b9dc
tiagoskaterock <tiagolemespalhano@gmail.com>
2024-02-23 03:57:37 -0300

    21. The front controller: get the resource, ID and the request method

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php 
 
-phpinfo();
+$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+$parts = explode('/', $path);
+
+$resource = $parts['2'];
+
+$id = $parts[3] ?? null;
+
+echo $resource, ', ', $id;
+
+echo $_SERVER['REQUEST_METHOD'];
