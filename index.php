792d347ba0e1da01ee5ef3a6e75f26940c33060b
tiagoskaterock <tiagolemespalhano@gmail.com>
2024-02-23 04:28:00 -0300

    23. Set the HTTP status code: best practices

--- index.php
+++ index.php
@@ -4,10 +4,21 @@ $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
 $parts = explode('/', $path);
 
-$resource = $parts['2'];
+$resource = $parts[3];
 
-$id = $parts[3] ?? null;
+$id = $parts[4] ?? null;
 
-echo $resource, ', ', $id;
+echo "\n" . $resource, ', ', $id . "\n";
 
-echo $_SERVER['REQUEST_METHOD'];
+echo 'Method: ' . $_SERVER['REQUEST_METHOD'] . "\n";
+
+// echo "<h1>" . $resource . "</h1>";
+
+if ($resource != 'tasks') {	
+	// header("{$_SERVER['SERVER_PROTOCOL']} 404 Not Found");
+	http_response_code(404);
+	exit;
+}
+// else {
+// 	echo 'bosta';
+// }
