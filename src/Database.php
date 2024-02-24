34fd4eb86fe3082c6db1e74433ed556192e76f9e
tiagoskaterock <tiagolemespalhano@gmail.com>
2024-02-24 02:44:26 -0300

    32. Move the database connection data to a separate .env file

--- src/Database.php
+++ src/Database.php
@@ -8,10 +8,21 @@ class Database {
 		private string $username,
 		private string $password,
 	) {
+		$this->newConnection();
+	}	
 
+
+	private function newConnection() {
+		$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+		$dotenv->load();
+
+		$database = new Database($_ENV['DB_HOST'], $_ENV['DB_NAME'], $_ENV['DB_USER'], $_ENV['DB_PASSWORD']);
+
+		$database->getConnection();
 	}
 
-	function getConnection() : PDO {
+
+	private function getConnection() : PDO {
 		$dsn = "mysql:host={$this->hostname};dbname={$this->database};charset=utf8";
 
 		return new PDO($dsn, $this->username, $this->password, [
